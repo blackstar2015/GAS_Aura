dc2ae838a8e52372cf28d3aebdc920eb4f527964
Pradeep Pillai <ppillai05@gmail.com>
2024-11-18 16:40:36 -0800

    created widget controller and health and mana widgets

---
+++ Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
@@ -0,0 +1,17 @@
+// Copyright by Pradeep Pillai
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetController/AuraWidgetController.h"
+#include "OverlayWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UOverlayWidgetController : public UAuraWidgetController
+{
+	GENERATED_BODY()
+	
+};
