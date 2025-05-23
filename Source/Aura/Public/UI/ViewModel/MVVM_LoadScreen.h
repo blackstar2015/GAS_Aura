32bbce90f8ba072b222f212a072fe2b6e3797f99
Pradeep Pillai <ppillai@vfs.com>
2025-05-23 11:50:43 -0700

    Assigning events on button pressed in load menu

--- Source/Aura/Public/UI/ViewModel/MVVM_LoadScreen.h
+++ Source/Aura/Public/UI/ViewModel/MVVM_LoadScreen.h
@@ -22,11 +22,18 @@ public:
 
 	UFUNCTION(BlueprintPure)
 	UMVVM_LoadSlot* GetLoadSlotViewModelByIndex(int32 Index) const;
+	
+	UFUNCTION(BlueprintCallable)
+	void NewSlotButtonPressed(int32 Slot,const FString& EnteredName);
+	
+	UFUNCTION(BlueprintCallable)
+	void NewGameButtonPressed(int32 Slot);
 
+	UFUNCTION(BlueprintCallable)
+	void SelectSlotButtonPressed(int32 Slot);
 private:
 	UPROPERTY()
 	TMap<int32, UMVVM_LoadSlot*> LoadSlots;
-
 	UPROPERTY()
 	TObjectPtr<UMVVM_LoadSlot> LoadSlot_0;
 	UPROPERTY()
